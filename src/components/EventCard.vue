45eb64147f9b6c7be85de2722bbb463aea6f0b50
Robert Joscelyne <robert.joscelyne@gmail.com>
2020-01-11 17:23:47 +0000

    Initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,14 @@
+// eslint-disable-next-line vue/valid-template-root
+<template> </template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: "I want to be single"
+    };
+  }
+};
+</script>
+
+<style></style>
